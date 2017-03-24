62e81de4514ac2519c9bdd70f34e9eb98ac89d7a
JAlberss <johnalbers2018@u.northwestern.edu>
2017-03-24 15:38:06 -0500

    Added directory structure

--- S-1scraper.py
+++ S-1scraper.py
@@ -3,9 +3,13 @@ import requests
 import os
 import urllib2 #used for parsing the text file
 #TODO incorporate dates? Or check if the accessionnumber is already there then ignore
+# Make a directory with all the downloaded files in it.
 
 def main():
+    os.makedirs("data")
     urlRetriever()
+    raw_input()
+    os.rmdir("data")
 
 def urlRetriever():
     r = requests.get('https://www.sec.gov/cgi-bin/browse-edgar?company=&CIK=&type=S-1&owner=include&count=40&action=getcurrent')
@@ -22,23 +26,26 @@ def accessNumberRetriever(text):
     f = open("browse.txt","r") #used so that lines can be accessed by array index
     accessionnumbersfile = open("accessionnumbers.txt","a+")
     for line in f:
-        if "Accession Number" in line:
-            accessionnumbersfile.write(line.split("Accession Number: ")[1].split(" &nbsp")[0]+"\n") # returns only the accession numbers
+        if "/Archives" in line and ".txt" in line:  # and ".htm" in line: #attempt to reduce errors that may occur is company has .txt in the filing
+            urlarg = line.split("href=\"")[2].partition("\"")[0]  # strips out .txt expression from the get request
+            url = "https://www.sec.gov" + urlarg
+            accessionnumber = urlarg.rpartition("/")[2].split(".txt")[0]
+            print accessionnumber
+            accessionnumbersfile.write(accessionnumber)
+            downloadTextFileFromUrl(url,accessionnumber)
+
     f.close()
-    f = open("browse.txt", "r") # Got to start at the top again
-    for line in f:
-        if "/Archives" in line and ".txt" in line: # and ".htm" in line: #attempt to reduce errors that may occur is company has .txt in the filing
-            urlarg = line.split("href=\"")[2].partition("\"")[0] #strips out .txt expression from the get request
-            url = "https://www.sec.gov"+ urlarg
-            downloadTextFileFromUrl(url)
     accessionnumbersfile.close()
     os.remove("browse.txt")
     os.remove("accessionnumbers.txt")
 
 
-def downloadTextFileFromUrl(url):
-    print url
-    #response = urllib2.urlopen(url)
+def downloadTextFileFromUrl(url,id):
+    #id will act as file name for the dir
+    f = open("data/"+id+".txt","w")
+    response = urllib2.urlopen(url)
+    f.write(response.read())
+    raw_input()
     #print response.read()
     #raw_input()
 
