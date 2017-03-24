f2a162d82171e124ad25bfe22d766bc3e4b84a32
JAlberss <johnalbers2018@u.northwestern.edu>
2017-03-24 14:33:02 -0500

    better url finding things

--- S-1scraper.py
+++ S-1scraper.py
@@ -2,7 +2,7 @@
 import requests
 import os
 import urllib2 #used for parsing the text file
-
+#TODO incorporate dates? Or check if the accessionnumber is already there then ignore
 
 def main():
     urlRetriever()
@@ -12,23 +12,46 @@ def urlRetriever():
     accessNumberRetriever(r.text)
 
 def accessNumberRetriever(text):
-    #f = open("workfile.txt","w")
-    #f.write(text)
-    #f.close()
-    f = open("workfile.txt","r") #used so that lines can be accessed by array index
+    if not (os.path.exists("accessionnumbers.txt")):
+        print "not here, create it"
+        createfile = open("accessionnumbers.txt", "w")
+        createfile.close()
+    f = open("browse.txt","w")
+    f.write(text)
+    f.close()
+    f = open("browse.txt","r") #used so that lines can be accessed by array index
+    accessionnumbersfile = open("accessionnumbers.txt","a+")
     for line in f:
         if "Accession Number" in line:
-            fileRetrievalByAccessionNumber(line.split("Accession Number: ")[1].split(" &nbsp")[0]) # returns only the accession numbers
+            accessionnumbersfile.write(line.split("Accession Number: ")[1].split(" &nbsp")[0]+"\n") # returns only the accession numbers
     f.close()
+    f = open("browse.txt", "r") # Got to start at the top again
+    for line in f:
+        if "/Archives" in line and ".txt" in line: # and ".htm" in line: #attempt to reduce errors that may occur is company has .txt in the filing
+            urlarg = line.split("href=\"")[2].partition("\"")[0] #strips out .txt expression from the get request
+            url = "https://www.sec.gov"+ urlarg
+            downloadTextFileFromUrl(url)
+    accessionnumbersfile.close()
+    #raw_input()
+    os.remove("browse.txt")
+    os.remove("accessionnumbers.txt")
 
-def fileRetrievalByAccessionNumber(accessionNumber):
-    arg1 = accessionNumber.split('-')[0].lstrip("0")
+
+#def fileRetrievalByAccessionNumber(accessionNumber):
+    #arg1 = accessionNumber.split('-')[0].lstrip("0")
     #arg1 = arg1.lstrip("0")#first 7 digits of the accession number, and strip leading zeros
-    arg2 = accessionNumber.replace("-","")
-    #arg2 is the full accessionnumber without dashes
+    #arg2 = accessionNumber.replace("-","")
+    #arg2 is often the full accessionnumber without dashes
+
     #example "https://www.sec.gov/Archives/edgar/data/1684506/000168450617000002/0001684506-17-000002.txt""
-    url = "https://www.sec.gov/Archives/edgar/data/"+arg1+"/"+arg2+"/"+accessionNumber+".txt"
+    #url = "https://www.sec.gov/Archives/edgar/data/"+arg1+"/"+arg2+"/"+accessionNumber+".txt"
+    #downloadTextFileFromUrl(url)
+    #print url
+
+def downloadTextFileFromUrl(url):
     print url
+    response = urllib2.urlopen(url)
+    print response.read()
     raw_input()
 
 if __name__ == "__main__":
